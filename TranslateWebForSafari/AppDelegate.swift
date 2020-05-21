8a160eda536fbe4e96f70bcae41f748e056c8f03
Manabu Nakazawa <mshibanami+git@gmail.com>
2020-05-22 00:30:07 +1000

    Refactor like OCD

--- TranslateWebForSafari/AppDelegate.swift
+++ TranslateWebForSafari/AppDelegate.swift
@@ -4,16 +4,75 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+    var windowController: NSWindowController?
     
-    func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+    func applicationWillFinishLaunching(_ notification: Notification) {
+        setupMenuItems()
     }
     
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        setupWindow()
     }
 
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return true
+    }
+    
+    private func setupWindow() {
+        let window = NSWindow(contentViewController: ViewController())
+        window.titleVisibility = .hidden
+        window.titlebarAppearsTransparent = true
+        window.isMovableByWindowBackground = true
+        window.styleMask = [.titled, .miniaturizable, .closable]
+        windowController = NSWindowController(window: window)
+        windowController?.showWindow(self)
+    }
+    
+    private func setupMenuItems() {
+        let mainMenu = NSMenu(title: "MainMenu")
+        NSApp.mainMenu = mainMenu
+        
+        // swiftlint:disable line_length
+        let menuList: [(String, (NSMenu) -> ([NSMenuItem]))] = [
+            (L10n.appName, { (menu: NSMenu) in
+                [
+                    NSMenuItem(title: L10n.menuItemHideApp, action: #selector(NSApp.hide(_:)), keyEquivalent: "h"),
+                    NSMenuItem(title: L10n.menuItemHideOthers, action: #selector(NSApp.hideOtherApplications(_:)), keyEquivalent: "h", keyEquivalentModifierMask: [.command, .option]),
+                    NSMenuItem(title: L10n.menuItemShowAll, action: #selector(NSApp.unhideAllApplications(_:)), keyEquivalent: ""),
+                    NSMenuItem.separator(),
+                    NSMenuItem(title: L10n.menuItemQuitApp, action: #selector(NSApp.terminate(_:)), keyEquivalent: "q")
+                ]
+            }),
+            (L10n.menuItemWindow, { (menu: NSMenu) in
+                [
+                    NSMenuItem(title: L10n.menuItemClose, action: #selector(NSWindow.performClose(_:)), keyEquivalent: "w"),
+                ]
+            })
+        ]
+        
+        for (menuTitle, itemsListFunc) in menuList {
+            let menuItem = NSMenuItem(title: menuTitle, action: nil, keyEquivalent: "")
+            mainMenu.addItem(menuItem)
+            let submenu = NSMenu(title: menuTitle)
+            for i in itemsListFunc(submenu) {
+                submenu.addItem(i)
+            }
+            menuItem.submenu = submenu
+        }
+    }
+}
+
+extension NSMenu {
+    private func removeFromSuperMenuItem() {
+        let servicesMenu = NSApp.servicesMenu
+        let supermenu = servicesMenu?.supermenu
+        supermenu?.removeItem(at: (supermenu?.indexOfItem(withSubmenu: servicesMenu))!)
+    }
+}
+
+extension NSMenuItem {
+    public convenience init(title string: String, action selector: Selector?, keyEquivalent charCode: String, keyEquivalentModifierMask: NSEvent.ModifierFlags) {
+        self.init(title: string, action: selector, keyEquivalent: charCode)
+        self.keyEquivalentModifierMask = keyEquivalentModifierMask
     }
 }
---
+++ TranslateWebForSafari/Consts.swift
@@ -0,0 +1,13 @@
+//
+//  Consts.swift
+//  TranslateWebForSafari
+//
+//  Created by Manabu Nakazawa on 21/5/20.
+//  Copyright © 2020 Manabu Nakazawa. All rights reserved.
+//
+
+import Foundation
+
+enum Consts {
+    static let supportPageURL = URL(string: "https://github.com/mshibanami/TranslateWebForSafari")!
+}
